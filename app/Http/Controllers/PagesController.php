b6e11c1a0e1746e144e22ac2ed8fb8a5d12a4e66
tmtflx <timothyfelixivano@icloud.com>
2021-03-24 11:50:33 +0700

    Registrasi dan Login ref #1

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function aboutus()
+    {
+        return view('aboutus');
+    }
+    public function login()
+    {
+        return view('login');
+    }
+    public function register()
+        {
+        return view('register');
+    }
+    public function cariobat()
+        {
+        return view('function.cariobat');
+    }
+}
---
+++ resources/views/template/landingtemplate.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+
+  {{-- css --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link href="{{ 'resource/css/main.css' }}" rel="stylesheet">
+  <link href="{{ 'resource/css/form.css' }}" rel="stylesheet">
+
+  {{-- icon --}}
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+
+<body>
+  {{-- navbar --}}
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <img src="{{ 'resource/imgres/logo-kemenkes.png' }}" alt="hostvital-logo">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" aria-current="page" href="{{ '/' }}">Beranda</a>
+          <a class="nav-link" href="{{ '/aboutus' }}">Tentang Kami</a>
+          <a class="nav-link" href="{{ '/articles' }}">Artikel</a>
+          <a class="nav-link" href="{{ '/layanan' }}">Layanan</a>
+          <a class="nav-link login-link" href="{{ '/login' }}">Login/Register</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  {{-- content --}}
+  @yield('content')
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="{{ 'resource/imgres/logo.png' }}" alt="logo-hostvital" style="margin-top: 20px">
+        </div>
+        <div class="col-md-3">
+          <p style="margin-bottom: 0; font-weight: 600">Site Map</p>
+          <a class="f-link" href="#">FAQ</a><br>
+          <a class="f-link" href="#">Syarat & Ketentuan</a>
+        </div>
+        <div class="col-md-5" style="text-align: right">
+          <p style="margin-bottom: 0; font-weight: 600">Hubungi Kami</p>
+          <p class="address">Gedung Hostvital, Jl. Sukabirus. B32-33, Bandung 12345</p>
+          <p class="address">Phone: 021-6969-6969</p>
+          <p class="address">Email: help@hostvital.com</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
