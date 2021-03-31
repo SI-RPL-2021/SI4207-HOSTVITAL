10ebbc5c207d8cd9bee21f621caad4e78d5bda52
meutiaaulia <meutiaaf19@gmail.com>
2021-03-31 08:19:47 +0700

    about us dan index

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+
+  {{-- css --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <link href="{{ 'resource/css/main.css' }}" rel="stylesheet">
+</head>
+
+<body background="{{ 'resource/imgres/back-about.png' }}"
+  style="background-size:100%; background-repeat: no-repeat; min-height: 100vh">
+  {{-- navbar --}}
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <img src="{{ 'resource/imgres/logo-kemenkes.png' }}" alt="hostvital-logo">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" aria-current="page" href="{{ '/' }}">Beranda</a>
+          <a class="nav-link" href="{{ '/aboutus' }}">Tentang Kami</a>
+          <a class="nav-link" href="#skills">Artikel</a>
+          <a class="nav-link" href="#portfolio">Layanan</a>
+          <a class="nav-link login-link" href="#contact">Login/Register</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div>
+    <div class="container">
+      <div class="shadow card" style="margin: 3rem 1rem 3rem 1rem; padding: 3rem; border-radius: 10px">
+        <img src="{{ 'resource/imgres/logo-big.png' }}" style="max-width: 453px">
+        <p style="margin-top: 2rem">Kami berkomitmen memberi layanan kesehatan prima dengan menghadirkan layanan
+          kesehatan terdepan yang didukung
+          oleh teknologi medis dan sistem informasi rumah sakit digital terkini. Kami percaya, komitmen ini akan
+          menjamin layanan yang lebih baik dan lebih cepat, baik untuk diagnosis, tindakan korektif, maupun tindakan
+          penyelamatan jiwa.</p>
+        <div class="maps" >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.2988673283744!2d107.62834001537593!3d-6.974022670228457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6285c5b7da517%3A0x864485f26a388f95!2sTelkom%20University!5e0!3m2!1sid!2sid!4v1616249279197!5m2!1sid!2sid"
+            width="100%" height="400px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="{{ 'resource/imgres/logo.png' }}" alt="logo-hostvital" style="margin-top: 20px">
+        </div>
+        <div class="col-md-3">
+          <p style="margin-bottom: 0; font-weight: 600">Site Map</p>
+          <a class="f-link" href="#">FAQ</a><br>
+          <a class="f-link" href="#">Syarat & Ketentuan</a>
+        </div>
+        <div class="col-md-5" style="text-align: right">
+          <p style="margin-bottom: 0; font-weight: 600">Hubungi Kami</p>
+          <p class="address">Gedung Hostvital, Jl. Sukabirus. B32-33, Bandung 12345</p>
+          <p class="address">Phone: 021-6969-6969</p>
+          <p class="address">Email: help@hostvital.com</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,108 @@
+@extends('template/landingtemplate')
+
+@section('title', 'Hosvital: Home')
+
+@section('content')
+  {{-- header pict --}}
+  <img src="{{ 'resource/imgres/back-landing.png' }}" alt="welcome hostvital" width="100%">
+
+  {{-- content --}}
+  <div class="container" style="margin-top: 3rem">
+    <div class="row">
+      {{-- left content --}}
+      <div class="col-7">
+        <h3 class="siagacovid">#SIAGA COVID-19</h3>
+        <a class="button-c" href="#"><img class="button-covid" src="{{ 'resource/imgres/button-covid.png' }}" alt="check covid-19"></a>
+
+        <h3 style="margin-top: 2rem">Layanan</h3>
+        <div class="row">
+          <div class="col-md layanan">
+            <a class="btn-layanan" href="#" style="text-decoration: none;">
+              <div class="shadow mb-5 bg-body" style="border-radius: 10px">
+                <img class="img" src="{{ 'resource/imgres/a-dokter.png' }}" alt="">
+                <p class="desc">Cari Dokter</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md layanan">
+            <a class="btn-layanan" href="{{ '/cariobat' }}" style="text-decoration: none;">
+              <div class="shadow mb-5 bg-body" style="border-radius: 10px">
+                <img class="img" src="{{ 'resource/imgres/a-obat.png' }}" alt="">
+                <p class="desc">Cari Obat</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md layanan">
+            <a class="btn-layanan" href="{{ '/carirs' }}" style="text-decoration: none;">
+              <div class="shadow mb-5 bg-body" style="border-radius: 10px">
+                <img class="img" src="{{ 'resource/imgres/a-rs.png' }}" alt="">
+                <p class="desc">Cari Rumah Sakit</p>
+              </div>
+            </a>
+          </div>
+          <div class="col-md layanan">
+            <a class="btn-layanan" href="#" style="text-decoration: none;">
+              <div class="shadow mb-5 bg-body" style="border-radius: 10px">
+                <img class="img" src="{{ 'resource/imgres/a-bpjs.png' }}" alt="">
+                <p class="desc">Panduan BPJS</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {{-- right content --}}
+      <div class="col-5">
+        <div class="row">
+          <div class="col">
+            <h3>Artikel</h3>
+          </div>
+          <div class="col" style="text-align: right">
+            <a class="b-link" href="#">Lihat lebih banyak</a>
+          </div>
+        </div>
+        <div class="shadow card berita" style="margin: 1rem 0 3rem; border: 0; border-radius: 10px">
+          <div class="shadow card mb-3 pop" style="border: 0; border-radius: 10px">
+            <div class="row g-0">
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h6 class="card-title judul">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis debitis, laborum atque ad dolore, totam omnis dignissimos hic nemo modi laudantium sed similique veritatis nihil cum expedita vero beatae!</h6>
+                  <a class="b-link" href="#" style="font-size: 14px">baca selengkapnya</a>
+                </div>
+              </div>
+              <div class="col-md-4" style="overflow: hidden;">
+                <img src="{{ 'resource/imgres/dummy-berita.jpeg' }}" style="height: 125px; border-radius: 0 10px 10px 0" align="right">
+              </div>
+            </div>
+          </div>
+          <div class="shadow card mb-3 pop" style="border: 0; border-radius: 10px">
+            <div class="row g-0">
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h6 class="card-title judul">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis debitis, laborum atque ad dolore, totam omnis dignissimos hic nemo modi laudantium sed similique veritatis nihil cum expedita vero beatae!</h6>
+                  <a class="b-link" href="#" style="font-size: 14px">baca selengkapnya</a>
+                </div>
+              </div>
+              <div class="col-md-4" style="overflow: hidden;">
+                <img src="{{ 'resource/imgres/dummy-berita.jpeg' }}" style="height: 125px; border-radius: 0 10px 10px 0" align="right">
+              </div>
+            </div>
+          </div>
+          <div class="shadow card mb-3 pop" style="border: 0; border-radius: 10px">
+            <div class="row g-0">
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h6 class="card-title judul">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nobis debitis, laborum atque ad dolore, totam omnis dignissimos hic nemo modi laudantium sed similique veritatis nihil cum expedita vero beatae!</h6>
+                  <a class="b-link" href="#" style="font-size: 14px">baca selengkapnya</a>
+                </div>
+              </div>
+              <div class="col-md-4" style="overflow: hidden;">
+                <img src="{{ 'resource/imgres/dummy-berita.jpeg' }}" style="height: 125px; border-radius: 0 10px 10px 0" align="right">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
