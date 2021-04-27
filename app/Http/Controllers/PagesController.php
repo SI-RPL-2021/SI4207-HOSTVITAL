79d390baf2ee6be3bb4acdf2f31b6b85d6eca47d
jasmineaurely <aurelysj@gmail.com>
2021-04-27 22:38:59 +0700

    penyempurnaan page controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -28,24 +28,6 @@ class PagesController extends Controller
     public function bpjs()
         {
         return view('function.bpjs');
-        }
-    public function caridokter(Request $r)
-    {
-        $dataSearch = null;
-        if ($r->get("nama_dokter"))
-        {
-            $keyword = $r->get("nama_dokter");
-            $dataSearch = DB::select("SELECT * FROM dokter WHERE nama LIKE '%$keyword%'");
-        }
-        return view('function.caridokter',["dataSearch"=>$dataSearch,"keyword"=>$keyword ?? null]);
-    }
-    public function profiledokter($id)
-    {
-        $data = DB::select("SELECT * FROM dokter WHERE id = ?",[$id]);
-        return view('function.profiledokter',["data"=>$data[0]]);
-    }
-    
-    
-    
+        }  
     
 }
