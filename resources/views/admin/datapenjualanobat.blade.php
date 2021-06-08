13d660053723693c859378d85fecbb4a3e5e54d4
meutiaaulia <meutiaaf19@gmail.com>
2021-06-08 11:13:51 +0700

    Memperbaiki tampilan data penjualan obat admin ref #16

--- resources/views/admin/datapenjualanobat.blade.php
+++ resources/views/admin/datapenjualanobat.blade.php
@@ -125,7 +125,7 @@
                                     alt="">
                                     </div>
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Closee</button>
                                     </div>
                                     </div>
                                 </div>
